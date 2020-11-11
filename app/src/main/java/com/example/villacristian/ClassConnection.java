947b939d8ab0517fedfe696fb2ea16613fbdf4a2
trunkpetas <alejandrovillanuevamartinez@gmail.com>
2020-11-11 13:17:30 +0100

    Cambios en el diseño después del login, menu principal de la app

---
+++ app/src/main/java/com/example/villacristian/ClassConnection.java
@@ -0,0 +1,4 @@
+package com.example.villacristian;
+
+public class ClassConnection {
+}
--- app/src/main/java/com/example/villacristian/PruebaFuncional.java
+++ app/src/main/java/com/example/villacristian/PruebaFuncional.java
@@ -2,27 +2,36 @@ package com.example.villacristian;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 
 public class PruebaFuncional extends AppCompatActivity {
-    Button btnFuncional;
-    TextView txtView;
+EditText e;
+Button btnCodificacion;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prueba);
-        txtView = (TextView) findViewById(R.id.textview_first);
-        btnFuncional = (Button) findViewById(R.id.button_first);
-        btnFuncional.setOnClickListener(new View.OnClickListener() {
+        btnCodificacion = (Button)findViewById(R.id.button17) ;
+        e = (EditText)findViewById(R.id.editText);
+        btnCodificacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtView.setText("MAGICAMENTE HA CAMBIADO EL TEXTO XD");
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                e.setText(currentDateTimeString);
             }
         });
-    }
+
+    };
 }
